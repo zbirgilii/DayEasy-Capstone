d666abf4a067541d72cb7ed763ad5f3b4c7fdbd3
zbirgilii <zzeynepbirgili@gmail.com>
2022-02-28 20:17:38 -0500

    Error fix

--- App.js
+++ App.js
@@ -1,26 +1,74 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/Home.js';
-import LoginScreen from "./screens/Login.js";
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import { Button } from 'react-native';
+import {StyleSheet, Text, View } from 'react-native';
 
-const Stack = createNativeStackNavigator();
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  
+  
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: '325201293658-0b5v1iqfstvgbqkh5bdmt76j5n9j3ode.apps.googleusercontent.com',
+    iosClientId: '325201293658-0b5v1iqfstvgbqkh5bdmt76j5n9j3ode.apps.googleusercontent.com',
+    androidClientId: '325201293658-0b5v1iqfstvgbqkh5bdmt76j5n9j3ode.apps.googleusercontent.com',
+    webClientId: '325201293658-0b5v1iqfstvgbqkh5bdmt76j5n9j3ode.apps.googleusercontent.com',
+  });
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { authentication } = response;
+      }
+  }, [response]);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false}}/>
-        {/* <Stack.Screen nane="Home" conponent={HomeScreen} /> */}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+    <>
+    <><View style={styles.mainView}>
+      <View style={styles.basicview}>
+        <Text style={styles.basicText}>DayEasy Login Page</Text>
+      </View>
+      <View>
+        <Text>Username</Text>
+      </View>
+      <View>
+        <Text>Password</Text>
+      </View>
+      <View>
+        <Text>Forgot My Password</Text>
+      </View>
+      <View>
+        <Text>Login with</Text>
+      </View>
+    </View></>
+    <>
+        <Button
+          disabled={!request}
+          title="Login"
+          onPress={() => {
+            promptAsync();
+          } } /></>
+          </>  
+  )}
+
+const styles = StyleSheet.create({
+  mainView:{
+    flex:1,
+    paddingTop:50,
+    backgroundColor: '#81B29A',
+    alignItems:'center', //center x axis
+    justifyContent:'center', //center y axis
+  },
+  basicview:{
+    backgroundColor:'#3D405B',
+    width:'100%',
+    marginBottom:5
+  },
+  basicText:{
+    fontSize:20,
+    color:'#F4F1DE',
+    textAlign:'center',
+    padding:20
+  }
+})
 // export default App;
-
-// import React, { Component } from 'react';
-// import * as WebBrowser from 'expo-web-browser';
-// import * as Google from 'expo-auth-session/providers/google';
-// import {StyleSheet, Text, View,Button, Alert, TextInput} from 'react-native';
-// WebBrowser.maybeCompleteAuthSession();
-
