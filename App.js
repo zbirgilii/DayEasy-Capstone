52ea7a0cd2f0413a49dc085fd3094542b56e17d1
robyjeremiah <46535871+robyjeremiah@users.noreply.github.com>
2022-03-01 06:33:33 -0500

    me

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import * as React from 'react';
+<<<<<<< Updated upstream
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/Home.js';
 import LoginScreen from "./screens/Login.js";
+=======
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import { Button } from 'react-native';
+>>>>>>> Stashed changes
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+<<<<<<< Updated upstream
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -24,3 +31,28 @@ export default function App() {
 // import {StyleSheet, Text, View,Button, Alert, TextInput} from 'react-native';
 // WebBrowser.maybeCompleteAuthSession();
 
+=======
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+    iosClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+    androidClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+    webClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+  });
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { authentication } = response;
+      }
+  }, [response]);
+
+  return (
+    <Button
+      disabled={!request}
+      title="Login"
+      onPress={() => {
+        promptAsync();
+        }}
+    />
+  );
+}
+>>>>>>> Stashed changes
