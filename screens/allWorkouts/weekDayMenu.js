fdd096854dca0fa82b7e40f80d25116d267f9081
Deus <amadeusrm97@gmail.com>
2022-04-22 09:30:45 -0400

    oopies

--- screens/allWorkouts/weekDayMenu.js
+++ screens/allWorkouts/weekDayMenu.js
@@ -65,7 +65,7 @@ export default function weekDayMenu() {
           userMuscle : muscleGroup,           
         })        
       }
-      //navigation.push("WorkoutSelect");      
+      navigation.push("WorkoutSelect");      
     })    
   }
   return (
